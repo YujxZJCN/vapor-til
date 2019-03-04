5d72de9c60e07aba04f69aebac05c5ccb93e8c92
俞佳兴 <alberty@yujiaxingdeMacBook-Pro.local>
2019-03-04 12:54:24 +0800

    first commit

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -11,10 +11,14 @@ public func routes(_ router: Router) throws {
     router.get("hello") { req in
         return "Hello, world!"
     }
-
+    
     // Example of configuring a controller
-    let todoController = TodoController()
-    router.get("todos", use: todoController.index)
-    router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
+    router.post("api", "acronyms") { req -> Future<Acronym> in
+        // 2
+        return try req.content.decode(Acronym.self)
+            .flatMap(to: Acronym.self) { acronym in
+                // 3
+                return acronym.save(on: req)
+        }
+    }
 }
